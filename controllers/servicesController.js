193c931c581ca74ff34364d59b316616106980d3
Mateo <mateobetancurb@gmail.com>
2023-09-24 18:13:23 -0500

    get services by id - controller

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { services } from "../data/services.js";
 import Services from "../models/Services.js";
 
@@ -18,8 +19,28 @@ const createService = async (req, res) => {
 	}
 };
 
-const getServices = (req, res) => {
+const getAllServices = (req, res) => {
 	res.json(services);
 };
 
-export { createService, getServices };
+const getServiceById = async (req, res) => {
+	const { id } = req.params;
+	//validar un object id
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		const error = new Error("El id no es válido");
+		return res.status(400).json({ msg: error.message });
+	}
+
+	//validar que el servicio existe
+	const service = await Services.findById(id);
+	if (!service) {
+		const error = new Error("El servicio no existe");
+		return res.status(404).json({ msg: error.message });
+	}
+
+	//mostrar el servicio
+	res.json(service);
+};
+
+export { createService, getAllServices, getServiceById };
