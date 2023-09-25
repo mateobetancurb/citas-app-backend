dffc5b9fefcf2d849acce4819e28f7cb94013079
Mateo <mateobetancurb@gmail.com>
2023-09-25 09:34:47 -0500

    validar cuando un servicio no existe

--- helpers/index.js
+++ helpers/index.js
@@ -8,4 +8,9 @@ function validateObjectId(id, res) {
 	}
 }
 
-export { validateObjectId };
+function handleNotFoundError(message, res) {
+	const error = new Error(message);
+	return res.status(404).json({ msg: error.message });
+}
+
+export { validateObjectId, handleNotFoundError };
