7575f88e76cdf4ba2c31deca65587dcf4373266b
Mateo <mateobetancurb@gmail.com>
2023-09-25 10:52:46 -0500

    insertando registros de forma masiva

--- data/seed.js
+++ data/seed.js
@@ -1,5 +1,21 @@
-function seedDB() {
-	console.log("desde seed");
+import dotenv from "dotenv";
+import { db } from "../config/db.js";
+import Services from "../models/Services.js";
+import { services } from "./services.js";
+
+dotenv.config();
+
+await db();
+
+async function seedDB() {
+	try {
+		await Services.insertMany(services);
+		console.log("se agregaron los datos");
+		process.exit(0);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 }
 
 function clearDB() {
