c81aa2aad75cf0cef50a7569c3d7210848009168
Mateo <mateobetancurb@gmail.com>
2023-09-25 08:56:10 -0500

    update service - route

--- routes/servicesRoutes.js
+++ routes/servicesRoutes.js
@@ -3,6 +3,7 @@ import {
 	createService,
 	getAllServices,
 	getServiceById,
+	updateService,
 } from "../controllers/servicesController.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/", createService);
 router.get("/", getAllServices);
 router.get("/:id", getServiceById);
+router.put("/:id", updateService);
 
 export default router;
