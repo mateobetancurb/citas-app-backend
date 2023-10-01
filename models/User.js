3b95a0fed37ed42f4d6c930b78ca4477fd9a24ab
Mateo <mateobetancurb@gmail.com>
2023-10-01 11:10:34 -0500

    users model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { generateId } from "../helpers";
+import { generateId } from "../helpers/index.js";
 
 const userSchema = mongoose.Schema({
 	name: {
@@ -20,7 +20,7 @@ const userSchema = mongoose.Schema({
 	},
 	token: {
 		type: String,
-		default: () => generateId,
+		default: () => generateId(),
 	},
 	verified: {
 		type: Boolean,
