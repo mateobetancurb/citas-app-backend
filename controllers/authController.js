1495a219325246c03e45500594765c80a602374f
Mateo <mateobetancurb@gmail.com>
2023-10-01 11:18:03 -0500

    evitando registros duplicados

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,20 @@
 import User from "../models/User.js";
 
 const userRegister = async (req, res) => {
+	//validar todos los campos
 	if (Object.values(req.body).includes("")) {
 		const error = new Error("Todos los campos son obligatorios");
+
+		return res.status(400).json({
+			msg: error.message,
+		});
+	}
+
+	const { name, email, password } = req.body;
+	//evitar registros duplicados
+	const userExists = await User.findOne({ email });
+	if (userExists) {
+		const error = new Error("Este usuario ya existe");
 
 		return res.status(400).json({
 			msg: error.message,
