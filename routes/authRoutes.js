3326afc4377477931b239c071791adf473f88e01
Mateo <mateobetancurb@gmail.com>
2023-10-03 13:38:05 -0500

    login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
 	userRegister,
 	verifyUserAccount,
+	userLogin,
 } from "../controllers/authController.js";
 
 const router = Router();
 
 router.post("/register", userRegister);
 router.get("/verify/:token", verifyUserAccount);
+router.post("/login", userLogin);
 
 export default router;
