60cdfb102226670e2d59bc8a632bccdaafb192a9
Mateo <mateobetancurb@gmail.com>
2023-10-07 15:36:59 -0500

    implementando middleware en rutas protegidas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,17 @@ import {
 	userRegister,
 	verifyUserAccount,
 	userLogin,
+	user,
 } from "../controllers/authController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.post("/register", userRegister);
 router.get("/verify/:token", verifyUserAccount);
 router.post("/login", userLogin);
+
+// rutas protegidas
+router.get("/user", authMiddleware, user);
 
 export default router;
