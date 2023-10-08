1472e7d1605da468f0e85a9e9ee73d70dc7c02b7
Mateo <mateobetancurb@gmail.com>
2023-10-08 12:34:38 -0500

    Get appointments by date

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,3 +1,4 @@
+import { parse, formatISO, startOfDay, endOfDay, isValid } from "date-fns";
 import Appointment from "../models/Appointment.js";
 
 const createAppointment = async (req, res) => {
@@ -19,4 +20,27 @@ const createAppointment = async (req, res) => {
 	}
 };
 
-export { createAppointment };
+const getAppointmentsByDate = async (req, res) => {
+	const { date } = req.query;
+	const newDate = parse(date, "dd/MM/yyyy", new Date());
+
+	if (!isValid(newDate)) {
+		const error = new Error("Error: la fecha no es válida");
+		return res.status(400).json({
+			msg: error.message,
+		});
+	}
+
+	const isoDate = formatISO(newDate);
+
+	const appointments = await Appointment.find({
+		date: {
+			$gte: startOfDay(new Date(isoDate)),
+			$lte: endOfDay(new Date(isoDate)),
+		},
+	}).select("time");
+
+	res.json(appointments);
+};
+
+export { createAppointment, getAppointmentsByDate };
