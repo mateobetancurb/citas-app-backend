0fbb5ba00ae29e52321a1cbfed9428ee2a05adbc
Mateo <mateobetancurb@gmail.com>
2023-10-11 13:07:59 -0500

    edit appointment by id

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -3,6 +3,7 @@ import {
 	createAppointment,
 	getAppointmentsByDate,
 	getAppointmentById,
+	updateAppointment,
 } from "../controllers/appointmentController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
@@ -10,5 +11,6 @@ const router = Router();
 router.post("/create-appointment", authMiddleware, createAppointment);
 router.get("/create-appointment", authMiddleware, getAppointmentsByDate);
 router.get("/edit-appointment/:id", authMiddleware, getAppointmentById);
+router.put("/edit-appointment/:id", authMiddleware, updateAppointment);
 
 export default router;
