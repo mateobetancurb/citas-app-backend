37ef0f118b757201df603cc396b27dbe79fe32cd
Mateo <mateobetancurb@gmail.com>
2023-10-11 08:57:09 -0500

    get appointment by id

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
 	createAppointment,
 	getAppointmentsByDate,
+	getAppointmentById,
 } from "../controllers/appointmentController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = Router();
 router.post("/create-appointment", authMiddleware, createAppointment);
 router.get("/create-appointment", authMiddleware, getAppointmentsByDate);
+router.get("/edit-appointment/:id", authMiddleware, getAppointmentById);
 
 export default router;
