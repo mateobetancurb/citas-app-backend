e149b97c27875df6c90c3b63a1cf9a9055e8398d
Mateo <mateobetancurb@gmail.com>
2023-10-11 13:07:55 -0500

    edit appointment by id

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -66,4 +66,42 @@ const getAppointmentById = async (req, res) => {
 	res.json({ appointment });
 };
 
-export { createAppointment, getAppointmentsByDate, getAppointmentById };
+const updateAppointment = async (req, res) => {
+	const { id } = req.params;
+
+	//validar que sea un id valido
+	if (validateObjectId(id, res)) return;
+
+	//validar que la cita exista
+	const appointment = await Appointment.findById(id).populate("services");
+
+	if (!appointment) {
+		return handleNotFoundError("La cita no existe", res);
+	}
+
+	//validar que la cita pertenezca al usuario logueado
+	if (appointment.user.toString() !== req.user._id.toString()) {
+		const error = new Error("Permiso denegado");
+		return res.status(403).json({ msg: error.message });
+	}
+
+	const { date, time, totalAmount, services } = req.body;
+	appointment.date = date;
+	appointment.time = time;
+	appointment.totalAmount = totalAmount;
+	appointment.services = services;
+
+	try {
+		const result = await appointment.save();
+		res.json({ msg: "Cita editada correctamente" });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export {
+	createAppointment,
+	getAppointmentsByDate,
+	getAppointmentById,
+	updateAppointment,
+};
