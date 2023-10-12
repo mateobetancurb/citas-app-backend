c223a2fe2db96e73026fffdc50639f3c4be30934
Mateo <mateobetancurb@gmail.com>
2023-10-12 18:59:04 -0500

    enviando correo al editar y eliminar una cita

--- emails/appointmentEmailService.js
+++ emails/appointmentEmailService.js
@@ -22,3 +22,51 @@ export async function sendEmailNewAppointment({ date, time }) {
 
 	console.log("message was sent", info.messageId);
 }
+
+export async function sendEmailUpdateAppointment({ date, time }) {
+	const transporter = createTransport(
+		process.env.MAILTRAP_HOST,
+		process.env.MAILTRAP_PORT,
+		process.env.MAILTRAP_USER,
+		process.env.MAILTRAP_PASSWORD
+	);
+
+	//enviar email
+	const info = await transporter.sendMail({
+		from: "Citas App <citas@appsalon.com>",
+		to: "admin@appsalon.com",
+		subject: "Citas App - Cita modificada",
+		text: "Citas App - Cita modificada",
+		html: `
+      <p>Hola admin, una de tus próximas citas ha sido modificada por el usuario</p>
+      <p>La nueva cita será el ${date} a las ${time}</p>
+      <strong>Ingresa al panel de administrador para ver más información sobre este cambio</strong>
+    `,
+	});
+
+	console.log("message was sent", info.messageId);
+}
+
+export async function sendEmailDeleteAppointment({ date, time }) {
+	const transporter = createTransport(
+		process.env.MAILTRAP_HOST,
+		process.env.MAILTRAP_PORT,
+		process.env.MAILTRAP_USER,
+		process.env.MAILTRAP_PASSWORD
+	);
+
+	//enviar email
+	const info = await transporter.sendMail({
+		from: "Citas App <citas@appsalon.com>",
+		to: "admin@appsalon.com",
+		subject: "Citas App - Cita cancelada",
+		text: "Citas App - Cita cancelada",
+		html: `
+      <p>Hola admin, una de tus próximas citas ha sido cancelada por el usuario</p>
+      <p>La cita estaba programada para el ${date} a las ${time}</p>
+      <strong>Ingresa al panel de administrador para ver más información sobre este cambio</strong>
+    `,
+	});
+
+	console.log("message was sent", info.messageId);
+}
