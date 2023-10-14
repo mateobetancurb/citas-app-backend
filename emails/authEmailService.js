11f1c188ee1cdce177346105f9a905967a490902
Mateo <mateobetancurb@gmail.com>
2023-10-14 09:03:49 -0500

    restablecer contraseña

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -10,15 +10,39 @@ export async function sendEmailVerification({ name, email, token }) {
 
 	//enviar email
 	const info = await transporter.sendMail({
-		from: "Citas App <clientes@appsalon.com>",
+		from: "Barbería Susa <clientes@barberiasusa.com>",
 		to: email,
-		subject: "Confirma tu cuenta",
-		text: "Confirma tu cuenta",
+		subject: "Barbería Susa - Confirma tu cuenta",
+		text: "Barbería Susa - Confirma tu cuenta",
 		html: `
-      <p>Hola ${name}, confirma tu cuenta en CitasApp</p>
+      <p>Hola ${name}, confirma tu cuenta en Barbería Susa</p>
       <p>Tu cuenta está casi lista, sólo debes confirmarla en el siguiente link</p>
       <a href="${process.env.FRONTEND_URL}/api/auth/verify/${token}">Confirmar cuenta</a>
       <p>Si tú no creaste esta cuenta puedes ignorar este mensaje</p>
+    `,
+	});
+
+	console.log("message was sent", info.messageId);
+}
+
+export async function sendEmailPasswordReset({ name, email, token }) {
+	const transporter = createTransport(
+		process.env.MAILTRAP_HOST,
+		process.env.MAILTRAP_PORT,
+		process.env.MAILTRAP_USER,
+		process.env.MAILTRAP_PASSWORD
+	);
+
+	//enviar email
+	const info = await transporter.sendMail({
+		from: "Barbería Susa <clientes@barberiasusa.com>",
+		to: email,
+		subject: "Barbería Susa - Restablece tu contraseña",
+		text: "Barbería Susa - Restablece tu contraseña",
+		html: `
+      <p>Hola ${name}, dale clic al siguiente link para restablecer tu contraseña</p>
+      <a href="${process.env.FRONTEND_URL}/api/auth/olvide-contrasena/${token}">Cambiar contraseña</a>
+      <p>Si tú no solicitaste cambiar la contraseña puedes ignorar este mensaje</p>
     `,
 	});
 
